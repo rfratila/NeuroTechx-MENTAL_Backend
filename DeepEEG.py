e32d0717f617db3038e4da1e6e921da6e4912383
Robert Fratila <fratilarobert@hotmail.com>
2016-10-22 16:10:50 -0400

    Trains on more than one patient

--- DeepEEG.py
+++ DeepEEG.py
@@ -3,19 +3,33 @@ import scipy.io as sio
 import theano
 import lasagne
 import theano.tensor as T
+import os
 
 dataPath = '/home/rfratila/Desktop/MENTALdata/'
-inputMat = sio.loadmat('%scovertShiftsOfAttention_VPiac.mat'%dataPath)# got this from: http://bnci-horizon-2020.eu/database/data-sets
+#inputMat = sio.loadmat('%scovertShiftsOfAttention_VPiac.mat'%dataPath)# got this from: http://bnci-horizon-2020.eu/database/data-sets
 #inputMat = sio.loadmat('eeglab_data.set') #data is stored under inputMat['EEG']
+trainingSet =[]
+for patient in os.listdir(dataPath):
+	if patient.endswith('.mat'):
+		trainingSet.append(patient)
 
-trainOut = numpy.array([[1,0]]) #this will contain the actual state of the brain
+trainOut = numpy.array([[0,1]]*len(trainingSet), dtype='float32') #this will contain the actual state of the brain
 
 input_var = T.tensor4('input')
 y = T.dmatrix('truth')
 
-data = numpy.array(inputMat['data']['X'][0][0][:10000], dtype='float32') #of size (657660, 62)
+data =[]
+for patient in trainingSet:
+	temp = sio.loadmat('%s%s'%(dataPath,patient))
+
+	data.append([numpy.array(temp['data']['X'][0][0][:10000], dtype='float32')])
+
+data = numpy.vstack(data)
+
 channels = 62
-dimensions = (1,1,data.shape[0],data.shape[1]) #We have to specify the input size because of the dense layer
+#dimensions = (1,1,data.shape[0],data.shape[1]) #We have to specify the input size because of the dense layer
+dimensions = (1,1,data.shape[1],data.shape[2]) #We have to specify the input size because of the dense layer
+
 
 network = lasagne.layers.InputLayer(shape=dimensions,input_var=input_var)
 #These are the hidden layers mostly for demonstration purposes
@@ -25,12 +39,10 @@ network = lasagne.layers.Conv2DLayer(network, num_filters=10, filter_size=(5,5),
 network = lasagne.layers.DenseLayer(network, num_units=2, nonlinearity = lasagne.nonlinearities.softmax)
 
 #Trying to figure out how to extract the frequencies and time from the data
-trainIn = data.reshape([1,1] + list(data.shape))
+
 out = lasagne.layers.get_output(network)
 view_fn = theano.function([input_var], out)
-testInput = view_fn(trainIn)
-print testInput
-#import pudb; pu.db
+
 
 #---------------------------------For training------------------------------------------
 
@@ -53,13 +65,12 @@ testAcc = T.mean(T.eq(T.argmax(testPrediction, axis=1), T.argmax(y, axis=1)),dty
 
 validateFn = theano.function([input_var, y], [testLoss, testAcc])	 #check for error and accuracy percentage
 
-for i in xrange(5):
-
-	train_function(trainIn, trainOut)
+for i in xrange(10):
+	chooseRandomly = numpy.random.randint(data.shape[0])
+	trainIn = data[chooseRandomly].reshape([1,1] + list(data[chooseRandomly].shape))
+	#import pudb; pu.db
+	train_function(trainIn, numpy.expand_dims(trainOut[chooseRandomly], axis=0))
 	
-	error, accuracy = validateFn(trainIn, trainOut) 			     #pass modified data through network
+	error, accuracy = validateFn(trainIn, numpy.expand_dims(trainOut[chooseRandomly], axis=0)) 			     #pass modified data through network
 
 	print "error: ",error,"and accuracy: ", accuracy
-
-testInput = view_fn(trainIn)
-print testInput
