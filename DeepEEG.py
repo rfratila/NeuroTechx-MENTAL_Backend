830bd826d20e78978c6e8dbdfe26a668018bcf5b
Robert Fratila <fratilarobert@hotmail.com>
2016-10-30 15:18:47 -0400

    saving and loading of a model is now possible with the error and accuracyu being saved for visulaization later

--- DeepEEG.py
+++ DeepEEG.py
@@ -22,16 +22,17 @@ def getData(dataPath):
 			counter+=1
 	print "%i samples found"%counter
 
-	trainOut = numpy.array([[1,0]]*len(trainingSet), dtype='float32') #this will contain the actual state of the brain
+	trainOut = [[1,0],[0,1]]*len(trainingSet) #this will contain the actual state of the brain
 
 	data =[]
 	for patient in trainingSet:
 		temp = sio.loadmat('%s%s'%(dataPath,patient))
-
-		data.append(numpy.array(temp['data']['X'][0][0][:10000], dtype='float32'))
+		data.append(temp['data']['X'][0][0][:10000])
 
 	data = numpy.stack(data)
-	data = OrderedDict(input=numpy.array(data), truth=numpy.array(trainOut))
+	trainOut = numpy.stack(trainOut)
+	#import pudb; pu.db
+	data = OrderedDict(input=numpy.array(data, dtype='float32'), truth=numpy.array(trainOut, dtype='float32'))
 	return data
 
 def createNetwork(dimensions, input_var):
@@ -41,18 +42,16 @@ def createNetwork(dimensions, input_var):
 	#These are the hidden layers mostly for demonstration purposes
 	#network = lasagne.layers.DenseLayer(network, num_units=200, nonlinearity=lasagne.nonlinearities.rectify)
 	#network = lasagne.layers.DenseLayer(network, num_units=200, nonlinearity=lasagne.nonlinearities.rectify)
-	network = lasagne.layers.Conv2DLayer(network, num_filters=10, filter_size=(5,5), nonlinearity=lasagne.nonlinearities.rectify)
+	network = lasagne.layers.Conv2DLayer(network, num_filters=15, filter_size=(5,5), nonlinearity=lasagne.nonlinearities.rectify)
+	network = lasagne.layers.Conv2DLayer(network, num_filters=20, filter_size=(5,5), nonlinearity=lasagne.nonlinearities.rectify)
 	network = lasagne.layers.DenseLayer(network, num_units=2, nonlinearity = lasagne.nonlinearities.softmax)
 	return network
-
-
-#view_fn = theano.function([input_var], out)
-
 
 #---------------------------------For training------------------------------------------
 def createTrainer(network,input_var,y):
 	#output of network
 	out = lasagne.layers.get_output(network)
+	#view_fn = theano.function([input_var], out)
 
 	#get all parameters from network
 	params = lasagne.layers.get_all_params(network, trainable=True)
@@ -79,7 +78,7 @@ def createValidator(network, input_var, y):
 def saveModel(network,saveLocation='',modelName='brain1'):
 
 	networkName = '%s%s.npz'%(saveLocation,modelName)
-	numpy.savez(networkName, *lasagne.layers.get_all_param_values(self.network))
+	numpy.savez(networkName, *lasagne.layers.get_all_param_values(network))
 
 def loadModel(network, model='brain1.npz'):
 
@@ -92,25 +91,36 @@ def main():
 	dataPath = '/home/rfratila/Desktop/MENTALdata/'
 	input_var = T.tensor4('input')
 	y = T.dmatrix('truth')
+	trainFromScratch = True
 
 	print ("Gathering data..."),
 	data = getData(dataPath)
 	print ("Creating Network...")
-	network  = createNetwork((1,1,data['input'].shape[1],data['input'].shape[2]), input_var)
+	networkDimensions = (1,1,data['input'].shape[1],data['input'].shape[2])
+	network  = createNetwork(networkDimensions, input_var)
 	print ("Creating Trainer...")
 	trainer = createTrainer(network,input_var,y)
 	print ("Creating Validator...")
 	validator = createValidator(network,input_var,y)
 
-	iterations = 10
+	if not trainFromScratch:
+		print 'loading a previously trained model...\n'
+		network = loadModel(network,'testing123.npz')
+
+	iterations = 30
 	print ("Training for %s iterations"%iterations)
+	record = OrderedDict()
 	for i in xrange(iterations):
 		chooseRandomly = numpy.random.randint(data['input'].shape[0])
+		print 'Patient: %d'%chooseRandomly
 		trainIn = data['input'][chooseRandomly].reshape([1,1] + list(data['input'][chooseRandomly].shape))
-		#import pudb; pu.db
 		trainer(trainIn, numpy.expand_dims(data['truth'][chooseRandomly], axis=0))
 		
-		error, accuracy = validator(trainIn, numpy.expand_dims(data['truth'][chooseRandomly], axis=0)) 			     #pass modified data through network
-
+		error, accuracy = validator(trainIn, numpy.expand_dims(data['truth'][chooseRandomly], axis=0))			     #pass modified data through network
+		record['error'] = error
+		record['accuracy'] = accuracy
+		record['iteration'] = i
 		print "error: ",error,"and accuracy: ", accuracy
+	saveModel(network=network,modelName='testing123')
+
 main()
