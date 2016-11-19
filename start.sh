9e65f84ab0c068496fa72375980c2d19acb42025
atg.abhishek <atg.abhishek@gmail.com>
2016-11-19 01:21:28 -0500

    updated script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo "/start" | python user.py -p /dev/tty.usbserial-DB00J8RE --add abhi person vanessa window_size 1 recording_session_number 8 attentive
+echo "/start" | python user.py "$1"
 echo "********"
 exit 0
